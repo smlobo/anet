c01231e94fad3c3fd94910f95379972a566525ca
Mara Dragan <mara@west.nl>
2018-05-01 11:52:25 +0200

    NCI-Agency/anet#498: Simplify avatar component
    We removed the options which are not needed.

--- client/src/components/AvatarComponent.js
+++ client/src/components/AvatarComponent.js
@@ -5,7 +5,6 @@ import AvatarEditor from 'react-avatar-editor'
 export default class AvatarComponent extends React.Component {
   state = {
     image: 'avatar.jpg',
-    allowZoomOut: false,
     position: { x: 0.5, y: 0.5 },
     scale: 1,
     rotate: 0,
@@ -40,10 +39,6 @@ export default class AvatarComponent extends React.Component {
     this.setState({ scale })
   }
 
-  handleAllowZoomOut = ({ target: { checked: allowZoomOut } }) => {
-    this.setState({ allowZoomOut })
-  }
-
   rotateLeft = e => {
     e.preventDefault()
 
@@ -59,11 +54,6 @@ export default class AvatarComponent extends React.Component {
     })
   }
 
-  handleBorderRadius = e => {
-    const borderRadius = parseInt(e.target.value)
-    this.setState({ borderRadius })
-  }
-
   handleXPosition = e => {
     const x = parseFloat(e.target.value)
     this.setState({ position: { ...this.state.position, x } })
@@ -72,16 +62,6 @@ export default class AvatarComponent extends React.Component {
   handleYPosition = e => {
     const y = parseFloat(e.target.value)
     this.setState({ position: { ...this.state.position, y } })
-  }
-
-  handleWidth = e => {
-    const width = parseInt(e.target.value)
-    this.setState({ width })
-  }
-
-  handleHeight = e => {
-    const height = parseInt(e.target.value)
-    this.setState({ height })
   }
 
   logCallback(e) {
@@ -95,10 +75,6 @@ export default class AvatarComponent extends React.Component {
 
   handlePositionChange = position => {
     this.setState({ position })
-  }
-
-  handleDrop = acceptedFiles => {
-    this.setState({ image: acceptedFiles[0] })
   }
 
   render() {
@@ -131,73 +107,10 @@ export default class AvatarComponent extends React.Component {
           name="scale"
           type="range"
           onChange={this.handleScale}
-          min={this.state.allowZoomOut ? '0.1' : '1'}
+          min="0.1"
           max="2"
           step="0.01"
           defaultValue="1"
-        />
-        <br />
-        {'Allow Scale < 1'}
-        <input
-          name="allowZoomOut"
-          type="checkbox"
-          onChange={this.handleAllowZoomOut}
-          checked={this.state.allowZoomOut}
-        />
-        <br />
-        Border radius:
-        <input
-          name="scale"
-          type="range"
-          onChange={this.handleBorderRadius}
-          min="0"
-          max="100"
-          step="1"
-          defaultValue="0"
-        />
-        <br />
-        Avatar Width:
-        <input
-          name="width"
-          type="number"
-          onChange={this.handleWidth}
-          min="50"
-          max="400"
-          step="10"
-          value={this.state.width}
-        />
-        <br />
-        Avatar Height:
-        <input
-          name="height"
-          type="number"
-          onChange={this.handleHeight}
-          min="50"
-          max="400"
-          step="10"
-          value={this.state.height}
-        />
-        <br />
-        X Position:
-        <input
-          name="scale"
-          type="range"
-          onChange={this.handleXPosition}
-          min="0"
-          max="1"
-          step="0.01"
-          value={this.state.position.x}
-        />
-        <br />
-        Y Position:
-        <input
-          name="scale"
-          type="range"
-          onChange={this.handleYPosition}
-          min="0"
-          max="1"
-          step="0.01"
-          value={this.state.position.y}
         />
         <br />
         Rotate:
